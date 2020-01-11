12fde556bc3e268b6be7ac78544dc737349d94e1
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-11 23:30:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  sentence_array.each do |word| 
+  sentence_array.each do |word|
     backwards_sentence_arry.push(word.reverse)
   end
   p backwards_sentence_arry
