e3094c4a74ea85acb08cfb3fdcfa4a148f6fdaaa
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-11 23:30:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
+  backwards_sentence_array = []
   sentence_array.each do |word|
-    backwards_sentence_arry.push(word.reverse)
+    backwards_sentence_array.push(word.reverse)
   end
-  p backwards_sentence_arry
+  p backwards_sentence_array
 end
 
 reverse_each_word("Hello there, and how are you?")
